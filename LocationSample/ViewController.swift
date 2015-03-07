9ef0fa53b9a705aac5a327178e5a7910df24890a
Ryan Khalili <rykhalil4@gmail.com>
2015-03-06 16:04:57 -0800

    initial commit with sample code

--- LocationSample/ViewController.swift
+++ LocationSample/ViewController.swift
@@ -7,19 +7,83 @@
 //
 
 import UIKit
+import CoreLocation
+import CoreBluetooth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        createRegions()
+        
+//        let uuidString = "E15382BA-CF0B-4392-9007-36973CA99FF8"
+//        let uuid = NSUUID( UUIDString: uuidString )
+//        let beaconRegion = CLBeaconRegion( proximityUUID: uuid, identifier: "CSR beacon" )
+//        beaconRegion.notifyEntryStateOnDisplay = true
+//        
+//        if CLLocationManager.isMonitoringAvailableForClass( CLBeaconRegion.classForCoder() ) {
+//            locationManager.startRangingBeaconsInRegion( beaconRegion )
+//            locationManager.startMonitoringForRegion( beaconRegion )
+//            locationManager.requestStateForRegion( beaconRegion )
+//        }
+    }
+    
+    func createRegions() {
+        
+        let lat = 37.3983930
+        let lon = -121.9115680
+        let coordinate = CLLocationCoordinate2DMake( lat, lon )
+        let radius = 300.0
+        let identifier = "CSR San Jose"
+        let region = CLCircularRegion(center: coordinate, radius: radius, identifier: identifier)
+        locationManager.startMonitoringForRegion(region)
+    }
+    
+    func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: [AnyObject]!, inRegion region: CLBeaconRegion!) {
+        
+        for beacon in beacons {
+            println( beacon.UUIDString )
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didDetermineState state: CLRegionState, forRegion region: CLRegion!) {
+        
+        println( region )
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func locationManager(manager: CLLocationManager!, monitoringDidFailForRegion region: CLRegion!, withError error: NSError!) {
+        
+        
+        print( "monitoring failed with error: \(error)" )
+    }
+    
+    func locationManager(manager: CLLocationManager!, didEnterRegion region: CLRegion!) {
+        
+        print( "Entered region!" )
+    }
+    
+    func locationManager(manager: CLLocationManager!, didExitRegion region: CLRegion!) {
+        
+        print( "Exited region!" )
+    }
 
-
+    func locationManager(manager: CLLocationManager!, didUpdateToLocation newLocation: CLLocation!, fromLocation oldLocation: CLLocation!) {
+        
+        println( newLocation.coordinate.latitude )
+        println( newLocation.coordinate.longitude )
+    }
 }
 
